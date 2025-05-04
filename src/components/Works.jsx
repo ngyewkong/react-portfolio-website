be8898b6f205816db8edcfcbd05937923148a0d4
Ng Yew Kong <ngyewkong1996@gmail.com>
2025-05-04 17:26:56 +0800

    update ui & fix breaking ui on small devices

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -23,7 +23,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px]"
       >
         <div className="relative w-full h-[230px]">
           <img
@@ -94,4 +94,4 @@ const Works = () => {
   );
 };
 
-export default SectionWrapper(Works, "");
+export default SectionWrapper(Works, "projects");
